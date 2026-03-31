89318f901362434db493a1709d8d38294c2ccc61
metal <metalheadkrn@gmail.com>
2026-03-31 16:10:17 +0530

    Creating Node Group Roles and Node Groups for EKS

--- main.tf
+++ main.tf
@@ -25,6 +25,36 @@ resource "aws_iam_role_policy_attachment" "main-eks_vpc_resource_controller" {
   role       = aws_iam_role.main.name
 }
 
+resource "aws_iam_role" "node" {
+  name = "${local.name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
+
 resource "aws_eks_cluster" "main" {
   name     = "${var.env}-eks"
   role_arn = aws_iam_role.main.arn
@@ -35,3 +65,19 @@ resource "aws_eks_cluster" "main" {
 
 }
 
+resource "aws_eks_node_group" "main" {
+  for_each        = var.node_groups
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${local.name}-${each.key}-ng"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = each.value["instance_types"]
+  capacity_type   = each.value["capacity_type"]
+
+  scaling_config {
+    desired_size = each.value["size"]
+    max_size     = each.value["size"] + 5
+    min_size     = each.value["size"]
+
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,4 @@ variable "env" {}
 variable "component" {}
 variable "project_name" {}
 variable "subnet_ids" {}
+variable "node_groups" {}
