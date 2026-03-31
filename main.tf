cd5cec9cf8352e8db916f3dcb399ff0665f1edbb
metal <metalheadkrn@gmail.com>
2026-03-31 13:07:58 +0530

    Terraform fix

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "main" {
-  name = "${local.name}-eks-role"
+  name = "${local.name}-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
