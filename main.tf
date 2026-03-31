0aed791ff36e65745b299332be4dd8bbf7b2095a
metal <metalheadkrn@gmail.com>
2026-03-31 18:06:08 +0530

    Added name for node groups using launch templates tagging

--- main.tf
+++ main.tf
@@ -75,12 +75,12 @@ resource "aws_eks_cluster" "main" {
 
 resource "aws_launch_template" "main" {
   for_each        = var.node_groups
-  name_prefix     = "${local.name}-${each.key}-ng-asg"
+  name_prefix     = "${local.name}-${each.key}-ng"
 
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "${local.name}-${each.key}-ng-asg"
+      Name = "${local.name}-${each.key}-ng"
     }
   }
 }
