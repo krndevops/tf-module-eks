1a32b2fd31176edcfea6f7d1ea65dcf4134c2959
metal <metalheadkrn@gmail.com>
2026-05-03 18:12:12 +0530

    Creating OIDC Open ID Connect AND INDIVIDIIUAl roles for pods, external dns and cluster asg

--- iam-roles-for-pods.tf
+++ iam-roles-for-pods.tf
@@ -1,44 +1,44 @@
-# resource "aws_eks_addon" "pod_identity" {
-#   cluster_name = aws_eks_cluster.main.name
-#   addon_name   = "eks-pod-identity-agent"
-# }
-#
-# # 1. The IAM Role
-# resource "aws_iam_role" "pod" {
-#   name = "${local.name}-pod-role"
-#
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "pods.eks.amazonaws.com"
-#         }
-#         Action = [
-#           "sts:AssumeRole",
-#           "sts:TagSession"
-#         ]
-#       }
-#     ]
-#   })
-# }
-#
-# # 2. Attach AdministratorAccess to the Role
-# resource "aws_iam_role_policy_attachment" "admin_access" {
-#   role       = aws_iam_role.pod.name
-#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
-#
-# # 3. Create Associations for both service accounts
-# resource "aws_eks_pod_identity_association" "main" {
-#   for_each = toset(["default", "external-dns"])
-#
-#   cluster_name    = aws_eks_cluster.main.name
-#   namespace       = "default"
-#   service_account = each.value
-#   role_arn        = aws_iam_role.pod.arn
-# }
+resource "aws_eks_addon" "pod_identity" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "eks-pod-identity-agent"
+}
+
+# 1. The IAM Role
+resource "aws_iam_role" "pod" {
+  name = "${local.name}-pod-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+# 2. Attach AdministratorAccess to the Role
+resource "aws_iam_role_policy_attachment" "admin_access" {
+  role       = aws_iam_role.pod.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# 3. Create Associations for both service accounts
+resource "aws_eks_pod_identity_association" "main" {
+  for_each = toset(["default", "external-dns"])
+
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "default"
+  service_account = each.value
+  role_arn        = aws_iam_role.pod.arn
+}
 
 
 
